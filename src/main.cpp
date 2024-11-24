7cf742ae78c8c1dc28566d9136ccbb1d4bf4ed74
Drew <145530440+DrewRoss5@users.noreply.github.com>
2024-11-24 12:14:58 -0700

    Implement simple game play
    This is mostly to ensure that the tictactoe "engine" works properly before making the bot

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
 #include "board.h"
 
+void player_move(GameBoard& board, int symbol){
+    unsigned int row, col;
+    std::cout << "Player " << symbol << "\n\tRow: ";
+    std::cin >> row;
+    std::cout << "\tColumn: ";
+    std::cin >> col;
+    if (board.get_cell(row, col) != SYM_NONE || row > 2 || col > 2){
+        std::cout << "Invalid move" << std::endl;
+        player_move(board, symbol);
+        return;
+    }
+    board.update_cell(row, col, symbol);
+
+}
+
 int main(){
     GameBoard board;
-    board.update_cell(2, 2, SYM_X);
-    board.update_cell(1, 1, SYM_X);
-    board.update_cell(0, 0, SYM_X);
-    board.display();
-    std::cout << "Winner: " << SYMBOL_STR[board.check_win()] << std::endl;
+    int turn = 0;
+    int symbol = SYM_X;
+    while (turn < 9){
+        system("clear");
+        board.display();
+        player_move(board, symbol);
+        int winner = board.check_win();
+        if (winner){
+            system("clear");
+            board.display();
+            std::cout << "Winner: Player " << symbol << std::endl;
+            return 0;
+        }
+        turn++;
+        symbol = 3 ^ symbol;
+    }
 }
