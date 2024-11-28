181fb347fc5dee97763ec5761eaf678d3e7f6ee2
Drew <145530440+DrewRoss5@users.noreply.github.com>
2024-11-27 22:13:38 -0700

    Rollback to previous version

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,48 @@
 #include <iostream>
-#include "../inc/chess.h"
+#include "board.h"
+#include "bot.h"
+
+void player_move(GameBoard& board, int symbol){
+    unsigned int row, col;
+    std::cout << "Player " << symbol << "\n\tRow: ";
+    std::cin >> row;
+    std::cout << "\tColumn: ";
+    std::cin >> col;
+    if (board.get_cell(row, col) != SYM_NONE || row > 2 || col > 2){
+        std::cout << "Invalid move" << std::endl;
+        player_move(board, symbol);
+        return;
+    }
+    board.update_cell(row, col, symbol);
+
+}
 
 int main(){
-    ChessBoard board;
-    board.move_piece(1, 4, 3, 4, true);
-    board.move_piece(6, 4, 4, 4, true);   
-    
+    GameBoard board;
+    Bot bot(&board, SYM_O);
+    std::cout << "Bot intialized" << std::endl;
+    int turn {0};
     board.display();
+    while (turn < 9){
+        player_move(board, SYM_X);
+        system("clear");
+        board.display();
+        turn++;
+        if (board.check_win() != SYM_NONE){
+            std::cout << "Player wins" << std::endl;
+            return 0;
+        }
+        if (turn == 9)
+            break;
+        std::cout << "Bot turn:" << std::endl;
+        bot.take_turn();
+        system("clear");
+        board.display();
+        turn++;
+        if (board.check_win() != SYM_NONE){
+            std::cout << "Bot wins" << std::endl;
+            return 0;
+        }
+    }
+    std::cout << "Tie!" << std::endl;
 }
